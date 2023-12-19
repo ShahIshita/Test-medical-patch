2517a09a9d59c456de580fa66dc6fe2f54718c4b
Dharati Mirani <dharati.mirani@bacancy.com>
2023-12-19 18:51:51 +0530

    Added api files

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,16 @@
 import { createStore } from "vuex";
+import users from "./modules/users";
+import user_management from "./modules/user_management";
+import doctors from "./modules/doctors";
+import patientData from "./modules/patientData";
+import devices from "./modules/devices";
 
-export default createStore({});
+export default createStore({
+  modules: {
+    users,
+    userManagement: user_management,
+    doctors,
+    patientData,
+    devices,
+  },
+});
